16c6c905f63f4c04492d3299720638ad3f4e76f8
Philip Ting <pting.me+gh@gmail.com>
2023-04-22 12:30:26 -0700

    cleaned up unused code

--- islands/Timer.tsx
+++ islands/Timer.tsx
@@ -1,14 +1,8 @@
 import { TimerButton } from "../components/TimerButton.tsx";
 import { timerState } from "../utils/timerState.ts";
 
-export default function Timer(props: any) {
-  // const [time, setTime] = useState(60 * 20);
-  // const [isActive, setIsActive] = useState(false);
-  // const { state: { isActive, timeLeft } } = props;
-
-  // useInterval(() => setTime(time - 1), isActive ? 1000 : null);
-
-  const { actionLabel, status, currentTime, toggle } = timerState;
+export default function Timer() {
+  const { actionLabel, currentTime, toggle } = timerState;
   return (
     <TimerButton
       action={actionLabel.value}
