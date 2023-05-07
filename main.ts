83b453f4bfa1be08e8f69b7c49acf720a92386c0
Philip Ting <pting.me+gh@gmail.com>
2023-05-07 10:11:15 -0700

    migrated to fresh_postcss 0.2.0

--- main.ts
+++ main.ts
@@ -6,11 +6,13 @@
 
 import freshTwind from "$fresh/plugins/twind.ts";
 import { start } from "$fresh/server.ts";
-import freshPostcss from "$fresh_postcss/mod.ts";
+import freshPostcss, { process } from "$fresh_postcss/mod.ts";
 
 import manifest from "./fresh.gen.ts";
 import postcssConfig from "./postcss.config.ts";
 import twindConfig from "./twind.config.ts";
+
+await process(postcssConfig);
 
 await start(manifest, {
   plugins: [freshTwind(twindConfig), freshPostcss(postcssConfig)],
