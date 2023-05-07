1c68eef9d49545d282093c2449f3a1cd5bc0b3fd
Philip Ting <pting.me+gh@gmail.com>
2023-05-07 01:04:35 -0700

    added more postcss functionality, and updated fresh_postcss

--- postcss.config.ts
+++ postcss.config.ts
@@ -1,9 +1,14 @@
 import autoprefixer from "autoprefixer";
+import csso from "postcss-csso";
+import nesting from "postcss-nesting";
 
 const config = {
   plugins: [
     autoprefixer(),
+    nesting(),
+    csso(),
   ],
+  from: "./static/style.css",
 };
 
 export default config;
